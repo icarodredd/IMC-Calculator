4dc53c32ae16ee5f2b1c72f56d74b9431a6e7a36
unknown <icarog854@gmail.com>
2024-01-20 11:04:15 -0300

    initial

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="gradient-text">IMC Calculator</h1>
+    </div>
+  );
+}
+
+export default Header;
